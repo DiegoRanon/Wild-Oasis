4953b8872b06730fc1593771f23236d339b930ff
Konan <mastercelis8891@gmail.com>
2025-06-01 15:17:01 -0400

    Mutations: Deleting a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -2,10 +2,20 @@ import supabase from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
-  if(error) 
-  {
-    console.error("cabins could not be loaded")
-    throw new Error
+  if (error) {
+    console.error("Cabins could not be loaded");
+    throw new Error();
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error("Cabins could not be deleted");
+    throw new Error();
   }
 
   return data;
