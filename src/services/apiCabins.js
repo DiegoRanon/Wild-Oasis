2c29ba063cf034f99f1f43bdc2c983987eba49f3
Konan <mastercelis8891@gmail.com>
2025-06-01 15:56:32 -0400

    Creating a new Cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,11 +10,24 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error("Cabin could not be created");
+    throw new Error();
+  }
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
-    console.error("Cabins could not be deleted");
+    console.error("Cabin could not be deleted");
     throw new Error();
   }
 
